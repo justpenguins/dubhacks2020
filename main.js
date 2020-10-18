3ed32a644e20f0186db30138558a8ff428712af5
mrchkl <h6f3b@ugrad.cs.ubc.ca>
2020-10-18 06:10:23 -0700

    Fill in the details and we good

--- main.js
+++ main.js
@@ -3,6 +3,31 @@
   * This Javacript file would add some functions to the map and make
   * it interactive
   */
-(function() {
-  "use strict";
-})();
+function myMap() {
+    var myLatLng = new google.maps.LatLng(47.6050, -122.3344);
+    //TODO: add in all the different coordinates of all the facilities (setting them as consts may be better)
+
+    var mapProp= {
+        zoom: 15,
+        center: myLatLng
+      };
+
+    /* sets js in html*/
+    var map = new google.maps.Map(document.getElementById("googleMap"),
+    mapProp);
+
+    /* sets a floating marker*/
+    var marker = new google.maps.Marker({
+    position: myLatLng,
+    title: "Test Marker!"
+    });
+    //TODO: Adding different icons amy also be good.
+
+    marker.setMap(map);
+  }
+
+
+
+
+
+
